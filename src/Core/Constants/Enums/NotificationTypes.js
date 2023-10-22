a2e6e998c4957c12897bca811d3075bd3fc26ee9
GoktugSulun <goktug.sulun@hotmail.com>
2023-10-22 09:50:46 +0300

    Notification Types are created

---
+++ src/Core/Constants/Enums/NotificationTypes.js
@@ -0,0 +1,6 @@
+export default Object.freeze({
+   REQUEST_FOR_FRIENDSHIP: 1,
+   ADDED_FRIEND: 2,
+   LIKED_POST: 3,
+   COMMENTED_POST: 4
+});
--- src/Core/Constants/Enums/index.js
+++ src/Core/Constants/Enums/index.js
@@ -2,3 +2,4 @@ export { default as HttpMethodTypes } from './HttpMethodTypes';
 export { default as SagaTakeTypes } from './SagaTakeTypes';
 export { default as HttpResponseTypes } from './HttpResponseTypes';
 export { default as NotifierTypes } from './NotifierTypes';
+export { default as NotificationTypes } from './NotificationTypes';
