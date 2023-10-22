46fc4af7d1907cfa234f1cabc33ab3068375078e
GoktugSulun <goktug.sulun@hotmail.com>
2023-10-22 10:32:44 +0300

    Pngs are edited

--- src/assets/Pngs/Pngs.js
+++ src/assets/Pngs/Pngs.js
@@ -1,9 +1,18 @@
+//* Post Images
 import img1 from './img-1.jpg';
 import img2 from './img-2.jpg';
 import img3 from './img-3.jpg';
+//* User Images
+import goktug from './goktug.jpeg';
+import damla from './damla.jpg';
 
-export default [
+export const PostImages =  [
    img1,
    img2,
    img3
+];
+
+export const UserImages = [
+   goktug,
+   damla
 ];
