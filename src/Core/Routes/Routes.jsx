c7299b98317e2aa6ab58985cda569703cb6597c0
GoktugSulun <goktug.sulun@hotmail.com>
2023-12-03 11:22:14 +0300

    Scroll problem solved & lazy components are created

--- src/Core/Routes/Routes.jsx
+++ src/Core/Routes/Routes.jsx
@@ -1,23 +1,24 @@
-import { Route, Routes } from 'react-router-dom';
+import { Route, Routes, useLocation } from 'react-router-dom';
 import ProtectedRoute from './ProtectedRoute';
 import { useDispatch, useSelector } from 'react-redux';
-import { useEffect } from 'react';
+import { lazy, useEffect } from 'react';
 import Loading from '../Components/Loading/Loading';
 import { FullSizeLoadingWrapper } from '../Components/Pages/FullSizeLoadingWrapper.style';
 import { AppConfigSagaActions } from '../Store/AppConfig.saga';
 
 // PAGES
-import Dashboard from '../../Pages/Dashboard/Dashboard';
-import Login from '../../Pages/Login/Login';
-import Register from '../../Pages/Register/Register';
-import UserProfile from '../../Pages/UserProfile/UserProfile';
-import Notifications from '../../Pages/Notifications/Notifications';
-import Layout from '../../Pages/Layout/Layout';
-import Settings from '../../Pages/Settings/Settings';
+const Dashboard = lazy(() => import('../../Pages/Dashboard/Dashboard'));
+const Login = lazy(() => import('../../Pages/Login/Login'));
+const Register = lazy(() => import('../../Pages/Register/Register'));
+const UserProfile = lazy(() => import('../../Pages/UserProfile/UserProfile'));
+const Notifications = lazy(() => import('../../Pages/Notifications/Notifications'));
+const Layout = lazy(() => import('../../Pages/Layout/Layout'));
+const Settings = lazy(() => import('../../Pages/Settings/Settings'));
 
 const RouteList = () => {
   const token = localStorage.getItem('token');
   const dispatch = useDispatch();
+  const location = useLocation();
   const { authorizedUser } = useSelector((state) => state.AppConfig.init);
 
   useEffect(() => {
@@ -25,6 +26,10 @@ const RouteList = () => {
       dispatch(AppConfigSagaActions.getInit());
     }
   }, [authorizedUser?.id, token]);
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [location.pathname]);
 
   if (!authorizedUser?.id && token) {
     return (
