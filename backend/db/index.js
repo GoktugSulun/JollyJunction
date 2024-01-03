f4dc010aca78182034985e697b8efeaede732e13
GoktugSulun <goktug.sulun@hotmail.com>
2024-01-03 21:43:58 +0300

    DB config

--- backend/db/index.js
+++ backend/db/index.js
@@ -1,13 +1,10 @@
 import { JSONFilePreset } from 'lowdb/node';
-import path from 'path';
 
-const __dirname = path.resolve();
-
-export const postsDB = await JSONFilePreset(__dirname + '/db/posts.json', { posts: [] });
-export const usersDB = await JSONFilePreset(__dirname + '/db/users.json', { users: [] });
-export const commentsDB = await JSONFilePreset(__dirname + '/db/comments.json', { comments: [] });
-export const notificationsDB = await JSONFilePreset(__dirname + '/db/notifications.json', { notifications: [] });
-export const likesDB = await JSONFilePreset(__dirname + '/db/likes.json', { likes: [] });
-export const savesDB = await JSONFilePreset(__dirname + '/db/saves.json', { saves: [] });
-export const friendsDB = await JSONFilePreset(__dirname + '/db/friends.json', { friends: [] });
-export const advertisementsDB = await JSONFilePreset(__dirname + '/db/advertisements.json', { advertisements: [] });
+export const postsDB = await JSONFilePreset('./db/posts.json', { posts: [] });
+export const usersDB = await JSONFilePreset('./db/users.json', { users: [] });
+export const commentsDB = await JSONFilePreset('./db/comments.json', { comments: [] });
+export const notificationsDB = await JSONFilePreset('./db/notifications.json', { notifications: [] });
+export const likesDB = await JSONFilePreset('./db/likes.json', { likes: [] });
+export const savesDB = await JSONFilePreset('./db/saves.json', { saves: [] });
+export const friendsDB = await JSONFilePreset('./db/friends.json', { friends: [] });
+export const advertisementsDB = await JSONFilePreset('./db/advertisements.json', { advertisements: [] });
